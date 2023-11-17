9e16868edca92c2d505e29d0e0087eb3c61dcdca
= <mutkumese@gmail.com>
2023-11-17 16:19:23 +0300

    fixt all files

--- backend/index.js
+++ backend/index.js
@@ -4,21 +4,21 @@ const db = require("./db.js");
 const port = 8000;
 
 app.get('/food/:id', async (req, res) => {
-    const data = await db.execute("SELECT * FROM food WHERE FoodID = ?",[req.params.id]);
+    const data = await db.execute("SELECT * FROM food WHERE FoodID = ?", [req.params.id]);
     console.log(data);
     res.send(data[0]);
 });
 
 
 
-app.get('/food/', async (req, res) => {
+app.get('/food', async (req, res) => {
     const data = await db.execute("SELECT * FROM food");
     console.log(data);
     res.send(data[0]);
 });
 
 app.get('/', (req, res) => {
-    res.send('Selam beyler!');
+    res.send('Welcome to LifeSync API!');
 });
 
 
