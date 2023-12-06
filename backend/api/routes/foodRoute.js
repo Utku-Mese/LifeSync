4546bca3219ce5944261b8474482423cb92de828
Ömer Faruk Özler <omerozler8@gmail.com>
2023-12-06 19:40:03 +0300

    news.js oluşturma

--- backend/api/routes/foodRoute.js
+++ backend/api/routes/foodRoute.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const db = require("../../db");
 
+
 /*
 ===>TEST VERİSİ<==
 Hızlı test edebilmeniz için  expected .json file sample
