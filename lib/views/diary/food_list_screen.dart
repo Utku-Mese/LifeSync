0a3b6e00e36bafe98452c35ddc737be83e1366b2
= <mutkumese@gmail.com>
2023-12-10 22:58:22 +0300

    edd foodListTile widget

--- lib/views/diary/food_list_screen.dart
+++ lib/views/diary/food_list_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:life_sync/models/food_model.dart';
 import 'package:life_sync/utils/app_theme.dart';
 import '../../controller/food_controller.dart';
+import 'widgets/foodList/food_list_tile_view.dart';
 
 class FoodListScreen extends StatefulWidget {
   const FoodListScreen({
@@ -21,12 +22,18 @@ class _FoodListScreenState extends State<FoodListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: AppTheme.background,
-        appBar: AppBar(
-          shape: ShapeBorder.lerp(
+      backgroundColor: AppTheme.background,
+      appBar: AppBar(
+        iconTheme: const IconThemeData(color: AppTheme.nearlyWhite),
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomRight: Radius.circular(32),
+          ),
+        ),
+        /* shape: ShapeBorder.lerp(
             const RoundedRectangleBorder(
-              borderRadius: BorderRadius.vertical(
-                bottom: Radius.circular(30),
+              borderRadius: BorderRadius.horizontal(
+                right: Radius.circular(30),
               ),
             ),
             const RoundedRectangleBorder(
@@ -35,38 +42,41 @@ class _FoodListScreenState extends State<FoodListScreen> {
               ),
             ),
             1,
+          ), */
+        title: const Text(
+          'Yiyecekler',
+          textAlign: TextAlign.left,
+          style: TextStyle(
+            fontFamily: AppTheme.fontName,
+            fontWeight: FontWeight.w700,
+            fontSize: 22 + 6 - 6,
+            letterSpacing: 1.2,
+            color: AppTheme.nearlyWhite,
           ),
-          title: const Text(
-            'Yiyecekler',
-            style: TextStyle(color: Colors.black),
-          ),
-          backgroundColor: AppTheme.white,
         ),
-        body: FutureBuilder<List<Food>>(
-          future: _foodController.fetchFoods(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(child: CircularProgressIndicator());
-            } else if (snapshot.hasError) {
-              return Center(
-                child: Text('Could not retrieve data: ${snapshot.error}'),
-              );
-            } else {
-              final foods = snapshot.data;
-              return ListView.builder(
-                itemCount: foods?.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    onTap: () {},
-                    title: Text(foods![index].name!),
-                    subtitle: Text(foods[index].calories!.toString()),
-                    trailing: const Icon(Icons.add),
-                  );
-                },
-                padding: const EdgeInsets.only(bottom: 20.0),
-              );
-            }
-          },
-        ));
+        backgroundColor: AppTheme.nearlyDarkBlue,
+      ),
+      body: FutureBuilder<List<Food>>(
+        future: _foodController.fetchFoods(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text('Could not retrieve data: ${snapshot.error}'),
+            );
+          } else {
+            final foods = snapshot.data;
+            return ListView.builder(
+              itemCount: foods?.length,
+              itemBuilder: (context, index) {
+                return FoodListTile(foods: foods, index: index);
+              },
+              padding: const EdgeInsets.only(bottom: 20.0),
+            );
+          }
+        },
+      ),
+    );
   }
 }
---
+++ lib/views/diary/widgets/foodList/food_list_tile_view.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+import '../../../../models/food_model.dart';
+import '../../../../utils/app_theme.dart';
+
+class FoodListTile extends StatelessWidget {
+  const FoodListTile({
+    super.key,
+    required this.foods,
+    required this.index,
+  });
+
+  final List<Food>? foods;
+  final int index;
+
+  void _showDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(32.0),
+            ),
+          ),
+          content: const SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Center(child: Text('Yiyecek eklendi')),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Tamam'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Color _getCaloriesColor(int calories) {
+    if (calories < 50) {
+      return Colors.green.shade300;
+    } else if (calories < 70) {
+      return Colors.green.shade500;
+    } else if (calories < 100) {
+      return Colors.green.shade700;
+    } else if (calories < 120) {
+      return Colors.orange.shade300;
+    } else if (calories < 150) {
+      return Colors.orange.shade500;
+    } else if (calories < 200) {
+      return Colors.orange.shade700;
+    } else if (calories < 250) {
+      return Colors.red.shade400;
+    } else if (calories < 300) {
+      return Colors.red.shade500;
+    } else if (calories < 350) {
+      return Colors.red.shade700;
+    } else {
+      return Colors.red.shade900;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: () {
+        _showDialog(context);
+      },
+      leading: CircleAvatar(
+        radius: 25,
+        backgroundColor: _getCaloriesColor(foods![index].calories!),
+        child: Text(
+          '${foods![index].calories!} \nkcal',
+          style: const TextStyle(fontSize: 15, color: AppTheme.nearlyWhite),
+          textAlign: TextAlign.center,
+        ),
+      ),
+      title: Text(foods![index].name!, style: AppTheme.title),
+      subtitle: Text(
+        foods![index].type!,
+        style: const TextStyle(fontSize: 15),
+      ),
+      trailing: const Icon(Icons.add, color: AppTheme.nearlyDarkBlue),
+    );
+  }
+}
